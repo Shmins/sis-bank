41bde70ebb1a7b7b08c507567cc1b7855bb486cd
Pedro <pedro.alyson.123@gmail.com>
2023-09-05 13:53:28 -0300

    {Delete client}, {delete card, update card, search card} -> created routes

--- src/main/java/com/bank/app/entity/client/repository/ClientRepository.java
+++ src/main/java/com/bank/app/entity/client/repository/ClientRepository.java
@@ -2,6 +2,7 @@ package com.bank.app.entity.client.repository;
 
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import com.bank.app.entity.client.model.Client;
@@ -14,5 +15,6 @@ public interface ClientRepository extends MongoRepository<Client, String>{
     List<Client> findByEmail(String email);
     List<Client> findByCards(List<Card> cards);
     List<Client> findByNameComplete(String nameComplete);
-    
+    @Query(value = "{'cards.numberCard': ?0}" )
+    Client findByCard(String number);
 }
